3206748c76708c2dc83b965b10015fac4f10b02e
Brad Davidson <brad.davidson@rancher.com>
2023-12-11 19:20:54 +0000

    Revert "Add HTTP client update function to tracing library"
    This reverts commit f8f659e66c6ec56fef092dced085d129c0e67176.

--- remotes/docker/resolver.go
+++ remotes/docker/resolver.go
@@ -42,6 +42,7 @@ import (
 	remoteerrors "github.com/containerd/containerd/remotes/errors"
 	"github.com/containerd/containerd/tracing"
 	"github.com/containerd/containerd/version"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 var (
@@ -588,9 +589,12 @@ func (r *request) do(ctx context.Context) (*http.Response, error) {
 			return nil
 		}
 	}
-
-	tracing.UpdateHTTPClient(client, tracing.Name("remotes.docker.resolver", "HTTPRequest"))
-
+	client.Transport = otelhttp.NewTransport(
+		client.Transport,
+		otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+			return tracing.Name("remotes.docker.resolver", "HTTPRequest")
+		}),
+	)
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to do request: %w", err)
